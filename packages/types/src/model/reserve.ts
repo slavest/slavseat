01af0f713e495aebe90add9f628d34cd5ef4014e
한동진 <arkc1009@gmail.com>
2024-03-22 05:04:11 +0900

    [#36] Reserve Model 타입 수정
    alway 값에 따라 end 타입 분기

--- packages/types/src/model/reserve.ts
+++ packages/types/src/model/reserve.ts
@@ -1,10 +1,20 @@
 import { Model } from '..';
 
-export interface ReserveInfo {
+interface ReserveBase {
   id: number;
   user: Model.UserInfo;
   facility: Model.FacilityInfo;
   start: Date;
-  end: Date | null;
-  always: boolean;
 }
+
+interface Reserve extends ReserveBase {
+  always: false;
+  end: Date;
+}
+
+interface AlwaysReserve extends ReserveBase {
+  always: true;
+  end: null;
+}
+
+export type ReserveInfo = Reserve | AlwaysReserve;
