4a02332e05f0056507c4c9725289a7951373992e
한동진 <dgnm@estsecurity.com>
2024-04-04 13:54:06 +0900

    [#49] Drawer Shared 컴포넌트 화

--- apps/frontend/src/shared/components/Drawer/index.tsx
+++ apps/frontend/src/shared/components/Drawer/index.tsx
@@ -1,24 +1,40 @@
-import {
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  DrawerPortal,
-  DrawerTitle,
-  DrawerTrigger,
-  Drawer as Root,
-} from './Drawer';
+import React, { PropsWithChildren } from 'react';
 
-export const Drawer = Object.assign(Root, {
-  DrawerPortal,
-  DrawerOverlay,
-  DrawerTrigger,
-  DrawerClose,
-  DrawerContent,
-  DrawerHeader,
-  DrawerFooter,
-  DrawerTitle,
-  DrawerDescription,
-});
+import { Drawer as VaulDrawer } from 'vaul';
+
+import { cn } from '@/shared/utils/class.util';
+
+interface DrawerProps {
+  open: boolean;
+  useTrigger?: boolean;
+  onClose: () => void;
+}
+
+// TODO: Trigger를 쓰고 싶을때는... 나중에 생각 해보도록 하자...
+export function Drawer({
+  children,
+  open,
+  onClose,
+}: PropsWithChildren<DrawerProps>) {
+  return (
+    <VaulDrawer.Root open={open} onClose={onClose}>
+      <VaulDrawer.Portal>
+        <VaulDrawer.Overlay
+          className="fixed inset-0 bg-black/5"
+          onClick={onClose}
+        />
+        <VaulDrawer.Content
+          className={cn(
+            'fixed inset-x-0 bottom-0 z-50',
+            'max-w-[50rem] h-auto',
+            'flex flex-col',
+            'mx-auto p-8',
+            'bg-white rounded-t-2xl shadow-blur outline-none',
+          )}
+        >
+          {children}
+        </VaulDrawer.Content>
+      </VaulDrawer.Portal>
+    </VaulDrawer.Root>
+  );
+}
