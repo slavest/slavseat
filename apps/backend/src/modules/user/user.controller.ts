59f91aab57ddcf9adcec8df3c38906da53a890a7
Heavyrisem <heavyrisem@gmail.com>
2024-04-16 23:37:44 +0900

    [#112] feat: User 이름 검색 API 추가

--- apps/backend/src/modules/user/user.controller.ts
+++ apps/backend/src/modules/user/user.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
+import { AdminGuard } from '../auth/guard/admin.guard';
+import { AuthUserGuard } from '../auth/guard/auth-user.guard';
+import { SearchUserByNameRequestDto } from './dto/request/searchUserByName';
+import { User } from './entity/user.entity';
+import { UserService } from './user.service';
+
+@ApiTags('유저')
 @Controller('/api/user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('/search/name')
+  @UseGuards(AuthUserGuard)
+  @UseGuards(AdminGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: '유저 이름 검색' })
+  @ApiCreatedResponse({ type: User, isArray: true })
+  searchUserByName(
+    @Query() serachuserBynameRequestDto: SearchUserByNameRequestDto,
+  ) {
+    return this.userService.searchUserByName(
+      serachuserBynameRequestDto.name,
+    );
+  }
+}
