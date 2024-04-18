d1fd37f85c5895af91e8e857f1f5c8321a702491
heavyrisem <heavyrisem@gmail.com>
2024-04-18 20:59:55 +0900

    [#112] feat: admin 예약 취소 기능 추가

--- apps/frontend/src/features/admin/routes/AdminReserveManage.tsx
+++ apps/frontend/src/features/admin/routes/AdminReserveManage.tsx
@@ -1,10 +1,4 @@
-import React, {
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 import { ReactZoomPanPinchContentRef } from 'react-zoom-pan-pinch';
 
@@ -18,71 +12,24 @@ import { formatMMDDHHMM } from '@/shared/constants/date.constant';
 import { cn } from '@/shared/utils/class.util';
 
 import { Box } from '../components/Box';
+import {
+  ManageReserveMenu,
+  ManageReserveMenuActionType,
+} from '../components/Dropdown/ManageReserveMenu';
 import { CreateReserveModal } from '../components/Modal/CreateReserveModal';
+import { ViewFacilityInGrid } from '../components/Modal/ViewFacilityInGrid';
 import { useCancelReserveMutation } from '../hooks/cancel-reserve';
 import { useAdminAppStore } from '../stores/adminAppStore';
-
-const columns: ColumnType<Model.ReserveInfo>[] = [
-  { dataKey: 'user.name', headerContent: '사용자명', flexGrow: 1 },
-  {
-    dataKey: 'start',
-    headerContent: '시작 시간',
-    flexGrow: 1,
-    renderContent: ({ start }) => formatDate(start, formatMMDDHHMM),
-  },
-  {
-    dataKey: 'end',
-    headerContent: '종료 시간',
-    flexGrow: 1,
-    renderContent: ({ end }) =>
-      end ? formatDate(end, formatMMDDHHMM) : null,
-  },
-  {
-    dataKey: 'always',
-    headerContent: '예약 유형',
-    flexGrow: 1,
-    renderContent: ({ always }) => (
-      <span
-        className={cn({
-          'text-blue-600': !always,
-          'text-red-600': always,
-        })}
-      >
-        {always ? '고정석' : '시간차'}
-      </span>
-    ),
-  },
-  {
-    dataKey: 'createdAt',
-    headerContent: '예약 일시',
-    flexGrow: 1,
-    renderContent: ({ createdAt }) =>
-      format(createdAt, 'yyyy-MM-dd HH:MM:SS'),
-  },
-  {
-    dataKey: 'facility',
-    headerContent: '좌석 정보',
-    flexGrow: 1,
-    renderContent: ({ facility }) =>
-      `${facility.floor.name} ${facility.name}`,
-  },
-];
 
 export function AdminReserveManage() {
   const { setTitle } = useAdminAppStore();
   useEffect(() => setTitle('예약 관리'), [setTitle]);
 
-  const transformRef = useRef<ReactZoomPanPinchContentRef>(null);
-  const facilityElementRef = useRef<Record<number, HTMLDivElement>>(
-    {},
-  );
   const [date, setDate] = useState<Date>(new Date());
   const [searchUserName, setSearchUserName] = useState<string>('');
   const [searchFloorId, setSearchFloorId] = useState<number>();
-  const [selectedReserve, setSelectedReserve] =
-    useState<Model.ReserveInfo>();
-  const [adminReserveModalOpen, setAdminReserveModalOpen] =
-    useState(false);
+  const [adminReserveModalOpen, setAdminReserveModalOpen] = useState(false);
+  // const [viewFacilityInGrid, setViewFacilityInGrid] = useState<Model.FacilitySummary>();
 
   const { mutate: cancleReserveMutation } = useCancelReserveMutation({
     onSuccess: () => toast.success('예약이 취소되었습니다.'),
@@ -96,86 +43,100 @@ export function AdminReserveManage() {
     () =>
       reserveList
         ?.filter((reserve) =>
-          searchUserName === ''
-            ? true
-            : reserve.user.name.includes(searchUserName),
+          searchUserName === '' ? true : reserve.user.name.includes(searchUserName),
         )
         .filter((reserve) =>
-          searchFloorId === undefined
-            ? true
-            : reserve.facility.floor.id === searchFloorId,
+          searchFloorId === undefined ? true : reserve.facility.floor.id === searchFloorId,
         ),
     [reserveList, searchFloorId, searchUserName],
   );
 
-  const addFacilityRef = useCallback(
-    (id: number, ref: HTMLDivElement | null) => {
-      if (ref) facilityElementRef.current[id] = ref;
+  const handleReserveAction = useCallback(
+    (action: ManageReserveMenuActionType, reserve: Model.ReserveInfo) => {
+      if (action.type === 'cancleReserve') cancleReserveMutation(reserve.id);
     },
-    [],
+    [cancleReserveMutation],
   );
 
-  const handleClickReserve = useCallback(
-    (reserve: Model.ReserveInfo) => {
-      if (reserve.id === selectedReserve?.id) {
-        setSelectedReserve(undefined);
-        return;
-      }
-
-      const element = facilityElementRef.current[reserve.facility.id];
-      if (transformRef.current && element) {
-        setSelectedReserve(reserve);
-        transformRef.current.zoomToElement(element, 1, 150);
-      }
+  const columns: ColumnType<Model.ReserveInfo>[] = [
+    { dataKey: 'user.name', headerContent: '사용자명', flexGrow: 1 },
+    {
+      dataKey: 'start',
+      headerContent: '시작 시간',
+      flexGrow: 1,
+      renderContent: ({ start }) => formatDate(start, formatMMDDHHMM),
     },
-    [selectedReserve?.id],
-  );
-
-  const handleClickCancelReserve = useCallback(() => {
-    if (!selectedReserve) return alert('선택된 예약이 없습니다.');
-    cancleReserveMutation(selectedReserve.id);
-    setSelectedReserve(undefined);
-  }, [cancleReserveMutation, selectedReserve]);
+    {
+      dataKey: 'end',
+      headerContent: '종료 시간',
+      flexGrow: 1,
+      renderContent: ({ end }) => (end ? formatDate(end, formatMMDDHHMM) : null),
+    },
+    {
+      dataKey: 'always',
+      headerContent: '예약 유형',
+      flexGrow: 1,
+      renderContent: ({ always }) => (
+        <span
+          className={cn({
+            'text-blue-600': !always,
+            'text-red-600': always,
+          })}
+        >
+          {always ? '고정석' : '시간차'}
+        </span>
+      ),
+    },
+    {
+      dataKey: 'createdAt',
+      headerContent: '예약 일시',
+      flexGrow: 1,
+      renderContent: ({ createdAt }) => format(createdAt, 'yyyy-MM-dd HH:MM:SS'),
+    },
+    {
+      dataKey: 'facility',
+      headerContent: '좌석 정보',
+      flexGrow: 1,
+      renderContent: ({ facility }) => `${facility.floor.name} ${facility.name}`,
+    },
+    {
+      dataKey: '',
+      headerContent: '',
+      flexGrow: 0.3,
+      renderContent: (reserve) => (
+        <ManageReserveMenu onAction={(action) => handleReserveAction(action, reserve)} />
+      ),
+    },
+  ];
 
   return (
-    <div className="flex flex-col p-4 gap-4 h-[calc(100vh_-_var(--var-admin-header-height))] overflow-hidden">
+    <div className="flex h-[calc(100vh_-_var(--var-admin-header-height))] flex-col gap-4 overflow-hidden p-4">
       <div className="flex gap-4">
-        <Box
-          title="필터"
-          className="flex-1"
-          innerClassName={cn('flex gap-4')}
-          fullWidth
-        >
+        <Box fullWidth className="flex-1" innerClassName={cn('flex gap-4')} title="필터">
           <span>
             <div className="text-xs">날짜</div>
             <input
+              className="rounded-md border border-neutral-400 p-1 text-sm focus:outline-purple-600"
               type="date"
               value={format(date, 'yyyy-MM-dd')}
-              onChange={(e) =>
-                setDate(
-                  parse(e.target.value, 'yyyy-MM-dd', new Date()),
-                )
-              }
-              className="text-sm border border-neutral-400 rounded-md p-1 focus:outline-purple-600"
+              onChange={(e) => setDate(parse(e.target.value, 'yyyy-MM-dd', new Date()))}
             />
           </span>
 
           <span>
             <div className="text-xs">층</div>
             <select
-              className="border border-neutral-400 rounded-md text-sm p-1 focus:outline-purple-600"
+              className="rounded-md border border-neutral-400 p-1 text-sm focus:outline-purple-600"
               value={searchFloorId}
               onChange={(e) =>
-                setSearchFloorId(
-                  e.target.value ? Number(e.target.value) : undefined,
-                )
+                setSearchFloorId(e.target.value ? Number(e.target.value) : undefined)
               }
             >
-              <option value="" key="">
+              <option key="" value="">
                 전체
               </option>
               {allFloorSummary?.map((floor) => (
-                <option value={floor.id} key={floor.id}>
+                <option key={floor.id} value={floor.id}>
                   {floor.name}
                 </option>
               ))}
@@ -185,8 +146,8 @@ export function AdminReserveManage() {
           <span>
             <div className="text-xs">사용자명</div>
             <input
+              className="rounded-md border border-neutral-400 p-1 text-sm focus:outline-purple-600"
               type="text"
-              className="border border-neutral-400 rounded-md text-sm p-1 focus:outline-purple-600"
               value={searchUserName}
               onChange={(e) => setSearchUserName(e.target.value)}
             />
@@ -195,7 +156,7 @@ export function AdminReserveManage() {
 
         <Box title="동작">
           <button
-            className="px-4 py-2 text-white bg-violet-600 rounded-xl"
+            className="rounded-xl bg-violet-600 px-4 py-2 text-white"
             onClick={() => setAdminReserveModalOpen(true)}
           >
             새 예약
@@ -204,11 +165,7 @@ export function AdminReserveManage() {
       </div>
       {/* <ScrollArea> */}
       <div className="flex-1">
-        <Table
-          data={filteredReserveList}
-          columns={columns}
-          fillHeight
-        />
+        <Table fillHeight columns={columns} data={filteredReserveList} />
       </div>
       {/* </ScrollArea> */}
 
@@ -247,6 +204,7 @@ export function AdminReserveManage() {
         open={adminReserveModalOpen}
         onClose={() => setAdminReserveModalOpen(false)}
       />
+      {/* {viewFacilityInGrid && <ViewFacilityInGrid facilities={}/>} */}
     </div>
   );
 }
